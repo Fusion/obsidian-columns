a8cc2cee9031f5a1e36dfbab0da4eee9332e2528
Chris F Ravenscroft <chris@voilaweb.com>
2022-02-26 18:13:10 -0800

    First commit with new functionality

--- main.ts
+++ main.ts
@@ -12,30 +12,12 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	replacementText: string;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+		this.replacementText = '';
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
@@ -45,37 +27,36 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Note: this will not work properly if changes are made after
+		// the original evaluation as maintaining state does not happen
+		// past the original parsing...
+		this.registerMarkdownPostProcessor((el, ctx) => {
+			if(el.innerHTML == '<p>|-</p>') {
+				this.replacementText = '<table><tr><td style="border:0">';
+				el.innerHTML = '';
+				return;
+			}
+			if(el.innerHTML == '<p>-|-</p>') {
+				this.replacementText += '</td><td style="border:0">';
+				el.innerHTML = '';
+				return;
+			}
+			if(el.innerHTML == '<p>-|</p>') {
+				el.innerHTML = this.replacementText + '</td></tr></table>';
+				this.replacementText = '';
+				return;
+			}
+			if(this.replacementText != '') {
+				this.replacementText += el.innerHTML;
+				el.innerHTML = '';
+				return;
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
